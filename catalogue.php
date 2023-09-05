36bf50d700889b3d84d6baf6640b3d637e6cf5c7
JBSLOTWINSKI <jean-baptiste.slotwinski@le-campus-numerique.fr>
2023-09-05 10:37:12 +0200

    3.2 — Catalogue avec tableaux associatifs

--- catalogue.php
+++ catalogue.php
@@ -22,9 +22,43 @@
 
 <main>
     <?php
-$products = ["Canapé", "Fauteuil", "Guénael"]; ?>
-    <?php echo $products[0] ?>
+        $products = [
+                [
+                        'nom' => 'Loki',
+                        'prix' => '4000',
+                        'weight' => '1500',
+                        'discount' => '0',
+                        'picture_url' => 'https://medias.maisonsdumonde.com/images/ar_1:1,c_pad,f_auto,q_auto,w_354/v1/mkp/M21062135_1/canape-3-places-en-velours-vert-anglais-fixe-droit.jpg'
+                ],
+                [
+                        'nom' => 'Kilo',
+                        'prix' => '3500',
+                        'weight' => '1500',
+                        'discount' => '0',
+                        'picture_url' => 'https://medias.maisonsdumonde.com/images/ar_1:1,c_pad,f_auto,q_auto,w_354/v1/mkp/M21062135_1/canape-3-places-en-velours-vert-anglais-fixe-droit.jpg'
+                ],
+                [
+                        'nom' => 'Liko',
+                        'prix' => [4600, 4750, 5220, 4950],
+                        'weight' => '1500',
+                        'discount' => '0',
+                        'picture_url' => 'https://medias.maisonsdumonde.com/images/ar_1:1,c_pad,f_auto,q_auto,w_354/v1/mkp/M21062135_1/canape-3-places-en-velours-vert-anglais-fixe-droit.jpg'
+                ],
+                [
+                        'nom' => 'Kloi',
+                        'prix' => '4279',
+                        'weight' => '1500',
+                        'discount' => '0',
+                        'picture_url' => 'https://medias.maisonsdumonde.com/images/ar_1:1,c_pad,f_auto,q_auto,w_354/v1/mkp/M21062135_1/canape-3-places-en-velours-vert-anglais-fixe-droit.jpg'
+                ],
+        ]
+        ?>
 
+    <div>
+        <h3><?php echo $products[2]['nom']?></h3>
+        <p><?php echo $products[2]['prix'][3]?></p>
+        <img src="<?php echo $products[3]['picture_url']?>">
+    </div>
 
 
 </main>
